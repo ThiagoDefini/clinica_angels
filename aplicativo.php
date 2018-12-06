9c3a25326ef9280c6c5056f6b7837cf89ac6c14f
Edimar De Souza Santos <181710026@senacinformatica.br>
2018-12-06 12:03:06 -0200

    Inserção bg e formataçoes index

--- aplicativo.php
+++ aplicativo.php
@@ -17,7 +17,7 @@
         
         <br>
         <header align="center">
-            <button><h3>Acesse nosso App</h3></button>
+        <button><h1>Acesse nosso App</h1></button>
         </header>
         <br>
       
--- index.php
+++ index.php
@@ -1,24 +1,33 @@
+<?php
+    
+?>
 <!DOCTYPE html>
 
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Clinica Angels - Página Inicial</title>
+        
     </head>
     <body>
-        
-        <div style="text-align: right">
+       
+    <body style="background-image: url('fotos_site/background.jpg.jpg'); "/>
+        <br>
         <h1 align="center">Clinica Angels</h1> 
-        
-        <header align="center">
+      
+        <header align="right">
         <form action="entrar.php" method="POST" >
         <input type="text" name="txtLogin" required placeholder="E-mail ou CPF: " />
-        
+    
         <input type="password" name="txtSenha" placeholder="Senha: " required />  
         
         <input type="submit" value="Entrar" />
-        </form></header>
-        </div>
+        </form>
+        <br>
+        <a href="FrmCliente.php">
+            <button>Cadastre-se</button></a>
+        </header>
+        
         <hr><br>       
         
         <?php        
@@ -31,13 +40,10 @@
         
         <br>
         
-        <header align="center">
-            <a href="FrmCliente.php">
-                 <button>Cadastre-se</button></a>
-        </header>
+        
         
         <h3 align="center">Sobre</h5>
-        <h4 align="center">Escreva aqui coisas sobre a Angel Clinicas</h4>
+        <h4 align="center">Bem vindo a Clinica Angels</h4>
         
         
         
